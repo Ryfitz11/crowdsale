2b6609d3e94e7fd9afb72052f29fc74996e9e0af
Ryan Fitzgerald <ryfitz@ymail.com>
2024-04-25 17:16:44 -0600

    load contracts

--- src/components/App.js
+++ src/components/App.js
@@ -4,31 +4,83 @@ import {ethers} from 'ethers'
 
 import Navigation from "./Navigation";
 import Info from "./Info"
+import Loading from "./Loading";
+import Progress from "./Progress";
+
+import TOKEN_ABI from "../abis/Token.json"
+import CROWDSALE_ABI from "../abis/Crowdsale.json"
+
+import config from "../config.json"
 
 function App(){
+  const [provider, setProvider] = useState(null)
+  const [crowdsale, setCrowdsale] = useState(null)
 
   const [account, setAccount] = useState(null)
+  const [accountBalance, setAccountBalance] = useState(0)
+
+  const [price, setPrice] = useState(0)
+  const [maxTokens, setMaxTokens] = useState(0)
+  const [tokensSold, setTokensSold] = useState(0)
+
+  const [isLoading, setIsLoading] = useState(true)
+
 
   const loadBlockchainData = async ()=>{
+
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const token = new ethers.Contract(config[31337].token.address, TOKEN_ABI, provider)
+    const crowdsale = new ethers.Contract(config[31337].crowdsale.address, CROWDSALE_ABI, provider)
+    setCrowdsale(crowdsale)
 
     const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
     const account = ethers.utils.getAddress(accounts[0])
     setAccount(account)
 
+    const accountBlanance = ethers.utils.formatUnits(await token.balanceOf(account), 18)
+    setAccountBalance(accountBlanance)
+
+    const price = ethers.utils.formatUnits(await crowdsale.price(), 18)
+    setPrice()
+
+    const maxTokens = ethers.utils.formatUnits(await crowdsale.maxTokens(), 18)
+    setMaxTokens()
+
+    const tokensSold = ethers.utils.formatUnits(await crowdsale.tokensSold(), 18)
+    setTokensSold()
+
+    setIsLoading(false)
+
   }
 
   useEffect(() => {
+    if(isLoading) {
+      loadBlockchainData()
+    }
     loadBlockchainData()
-  });
+  }, [isLoading]);
   
 
   return(
     <Container>
       <Navigation/>
+
+      <h1 className="text-center my-4">Introducing DApp Token!</h1>
+
+      {isLoading ? (
+        <Loading/>
+      ):(
+        <>
+        <p className="text-center"><strong>Current Price:</strong> {price}</p>
+        <Progress maxTokens={maxTokens} tokensSold={tokensSold}/>
+        </>
+      )}
+
       <hr/>
       {account && (
-        <Info account={account}/>
+        <Info account={account} accountBalance={accountBalance}/>
       )}
     </Container>
   )
